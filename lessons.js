280822087a08135d2ce969f62d477b571af39b72
beni <116164939+avram-beni11@users.noreply.github.com>
2022-11-18 10:19:31 +0000

    added vue

---
+++ lessons.js
@@ -0,0 +1,93 @@
+var lessons = [
+    {
+        id: 01,
+        name: "Japanese",
+        description: "Japanese lessons tought by <em>professional</em> Japanese speakers!",
+        price: "£20",
+        location: "London",
+        spaces: 5,
+        image: "assets/japanese.png"
+    },
+    {
+        id: 02,
+        name: "English",
+        description: "English lessons tought by <em>professional</em> English speakers!",
+        price: "£15",
+        location: "Leeds",
+        spaces: 13,
+        image: "assets/english.png"
+    },
+    {
+        id: 03,
+        name: "Math",
+        description: "English lessons tought by <em>professional</em> English speakers!",
+        price: "£15",
+        location: "Leeds",
+        spaces: 13,
+        image: "assets/english.png"
+    },
+    {
+        id: 04,
+        name: "Science",
+        description: "English lessons tought by <em>professional</em> English speakers!",
+        price: "£15",
+        location: "Leeds",
+        spaces: 13,
+        image: "assets/english.png"
+    },
+    {
+        id: 05,
+        name: "Music",
+        description: "English lessons tought by <em>professional</em> English speakers!",
+        price: "£15",
+        location: "Leeds",
+        spaces: 13,
+        image: "assets/english.png"
+    },
+    {
+        id: 06,
+        name: "Information Technology",
+        description: "English lessons tought by <em>professional</em> English speakers!",
+        price: "£15",
+        location: "Leeds",
+        spaces: 13,
+        image: "assets/english.png"
+    },
+    {
+        id: 07,
+        name: "Programming",
+        description: "English lessons tought by <em>professional</em> English speakers!",
+        price: "£15",
+        location: "Leeds",
+        spaces: 13,
+        image: "assets/english.png"
+    },
+    {
+        id: 08,
+        name: "Art",
+        description: "English lessons tought by <em>professional</em> English speakers!",
+        price: "£15",
+        location: "Leeds",
+        spaces: 13,
+        image: "assets/english.png"
+    },
+    {
+        id: 09,
+        name: "Geography",
+        description: "English lessons tought by <em>professional</em> English speakers!",
+        price: "£15",
+        location: "Leeds",
+        spaces: 13,
+        image: "assets/english.png"
+    },
+    {
+        id: 10,
+        name: "History",
+        description: "English lessons tought by <em>professional</em> English speakers!",
+        price: "£15",
+        location: "Leeds",
+        spaces: 13,
+        image: "assets/english.png"
+    },
+
+]
